68661c66432b9b4816aeb911fa01cb315b52346f
geo-gs <georgemcarlson@gmail.com>
2022-03-06 18:35:30 +0000

    Add SCPRIME_WEB_WALLET_DATA_DIR Env Variable

---
+++ build/env.go
@@ -0,0 +1,7 @@
+package build
+
+const (
+	// EnvvarMetaDataDir is the environment variable that tells the web wallet where
+	// to put the sia data
+	EnvvarMetaDataDir = "SCPRIME_WEB_WALLET_DATA_DIR"
+)
--- cmd/scp-webwallet/config.go
+++ cmd/scp-webwallet/config.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"os"
-	"path/filepath"
-	"runtime"
 	"time"
 
 	"gitlab.com/scpcorp/ScPrime/node"
+	"gitlab.com/scpcorp/webwallet/build"
 )
 
 // createNodeParams parses the provided config and creates the corresponding
@@ -20,25 +18,8 @@ func configNodeParams() node.NodeParams {
 	params.CreateWallet = true
 	// Parse remaining fields.
 	params.Bootstrap = true // set to true when the gateway should use the bootstrap peer list
-	params.Dir = defaultScPrimeUIDir()
+	params.Dir = build.ScPrimeWebWalletDir()
 	params.APIaddr = "localhost:4300"
 	params.CheckTokenExpirationFrequency = 1 * time.Hour // default
 	return params
-}
-
-// defaultScPrimeUiDir returns the default data directory of scp-webwallet.
-// The values for supported operating systems are:
-//
-// Linux:   $HOME/.scprime-webwallet
-// MacOS:   $HOME/Library/Application Support/ScPrime-WebWallet
-// Windows: %LOCALAPPDATA%\ScPrime-WebWallet
-func defaultScPrimeUIDir() string {
-	switch runtime.GOOS {
-	case "windows":
-		return filepath.Join(os.Getenv("LOCALAPPDATA"), "ScPrime-WebWallet")
-	case "darwin":
-		return filepath.Join(os.Getenv("HOME"), "Library", "Application Support", "ScPrime-WebWallet")
-	default:
-		return filepath.Join(os.Getenv("HOME"), ".scprime-webwallet")
-	}
 }
